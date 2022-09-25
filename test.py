fa7c13fb57961d1fa90cacd7553f53c0518aa29f
ChristianAhlefeldt <112874093+ChristianAhlefeldt@users.noreply.github.com>
2022-09-25 14:42:45 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,150 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> windows = ifc.by_type('IfcWindow')
+... print('Number of windows:', len(windows))
+SyntaxError: multiple statements found while compiling a single statement
+>>> reset
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    reset
+NameError: name 'reset' is not defined
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> 
+================================ RESTART: Shell ================================
+>>> import ifcopenshell 
+... import bpy
+... import blenderbim.bim
+... import ifcopenshell.util
+... import ifcopenshell.util.element
+...         
+... from blenderbim.bim.ifc import IfcStore
+... ifc = ifcopenshell.open(IfcStore.path)
+... 
+... ######
+... # We use the script to find information about walls, roof, floor, windows and doors.
+... ######
+... 
+... 
+... ####
+... # Walls
+... ####
+... # Finding the walls and the amount    
+... walls = ifc.by_type('IfcWall')
+... print('Number of walls:', len(walls))
+... 
+
+# Finding the the properties for the walls, to find the walls, and properties, that matter for us
+for wall in walls:
+     wall.Name
+     for definition in wall.IsDefinedBy:
+
+         if definition.is_a('IfcRelDefinesByProperties'):
+             property_set = definition.RelatingPropertyDefinition
+
+             if property_set.Name == "Pset_WallCommon":
+                for property in property_set.HasProperties:
+
+                     #print(property)
+                     
+#Trying to define the external walls, because it is the only walls that matter for us
+                     if property.Name == "IsExternal":
+                        wall_ext=wall
+                        print(property.NominalValue.wrappedValue)
+
+
+
+# Finding the material for the walls
+# work in the progress
+#for wall in walls:
+         #for relAssociatesMaterialLayerSetUsage in wall.HasAssociations:
+             #print(relAssociatesMaterialLayerSetUsage.RelatingMaterial.ForLayerSet)
+                
+               # for relAssociatesMaterialLayerSet in wall.HasAssociations:
+                   # for relAssociatesMaterialLayer in wall.HasAssociations:
+                       #print(relAssociatesMaterialLayer.RelatingMaterial.ForMaterial)
+
+
+# For the walls we need to find the size
+
+####
+# Roof
+####
+
+roofs = ifc.by_type('IfcRoof')
+print('Number of roofs:', len(roofs))
+
+for roof in roofs:
+     roof.Name
+     for definition in roof.IsDefinedBy:
+
+         if definition.is_a('IfcRelDefinesByProperties'):
+             property_set = definition.RelatingPropertyDefinition
+
+             if property_set.Name == "Pset_RoofCommon":
+                for property in property_set.HasProperties:
+
+                     #print(property)
+                     
+##Finding the area of the roof
+                     if property.Name == "TotalArea":
+                        print('area =',property.NominalValue.wrappedValue)
+                     
+                     
+for roof in roofs:
+         for relAssociatesMaterialLayerSetUsage in roof.HasAssociations:
+             print(relAssociatesMaterialLayerSetUsage.RelatingMaterial.ForLayerSet)
+
+# Do the same about the material from the walls
+
+#############
+# We stoped here Because we know we have to do the same scripting for the rest, floors, 
+# windows and doors, but we didn't want to do it now, if the things we have done isn't 
+# the right way to do. So work in progress. Also we maybe need to look at the spaces to 
+# find the size of the building, but the size of the roof, can maybe be used for this.
+# For doors and windows, we also need to find out on which walls they are placed.
+#############
+
+
+####
+# Floors
+####
+
+floors = ifc.by_type('IfcSlab')
+print('Number of floors:', len(floors))
+
+####
+# Windows
+####
+
+windows = ifc.by_type('IfcWindow')
+print('Number of windows:', len(windows))
+
+####
+# Doors 
+####
+
+doors = ifc.by_type('IfcDoor')
+print('Number of doors:', len(doors))
+
+####
+# Spaces
+####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
